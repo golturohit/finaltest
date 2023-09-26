1453b37ba3390692420de4570f00ff766ab496a3
golturohit <rahulgarkha4@gmail.com>
2023-09-26 15:20:15 +0530

    search remove

--- src/layout/headers/HeaderOne.tsx
+++ src/layout/headers/HeaderOne.tsx
@@ -43,41 +43,7 @@ const HeaderOne = () => {
                   </div>
                   <div className="col-xxl-2 col-xl-2 col-lg-6 col-6">
                     <div className="tpheader__right d-flex align-items-center justify-content-end">
-                      <div className="search-btn-wrap">
-                        <button
-                          className={`button-search-toggle ${
-                            openSearch ? "tp-search-icon-active" : ""
-                          }`}
-                          onClick={() => setOpenSearch(!openSearch)}
-                        >
-                          <i className="header_search-button far fa-search d-none"></i>
-                          <i className="header_search-button">
-                            <svg
-                              width="20"
-                              height="20"
-                              viewBox="0 0 20 20"
-                              fill="none"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                d="M9.55 18.1C14.272 18.1 18.1 14.272 18.1 9.55C18.1 4.82797 14.272 1 9.55 1C4.82797 1 1 4.82797 1 9.55C1 14.272 4.82797 18.1 9.55 18.1Z"
-                                stroke="#271D3A"
-                                strokeWidth="1.5"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                              />
-                              <path
-                                d="M19.0001 19L17.2001 17.2"
-                                stroke="#271D3A"
-                                strokeWidth="1.5"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                              />
-                            </svg>
-                          </i>
-                          <i className="header_search-close far fa-times"></i>
-                        </button>
-                      </div>
+                      
                       <div className="tpheader__btn ml-25 d-none d-sm-block">
                         <Link href="/contact" className="tp-header-btn">
                           Get a Quote
